e3521847c6d902db026523dc1f34117672d98d61
utkarsh032 <utkarshraj525@gmail.com>
2024-02-19 15:16:20 +0530

    Starter

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+
+  return (
+    <div>
+      <h1 className="h-screen">Hello Coders</h1>
+      <h1 className="h-screen">Hello Coders</h1>
+      <h1 className="h-screen">Hello Coders</h1>
+      <h1 className="h-screen">Hello Coders</h1>
+      <h1 className="h-screen">Hello Coders</h1>
+    </div>
+  )
+}
+
+export default App
